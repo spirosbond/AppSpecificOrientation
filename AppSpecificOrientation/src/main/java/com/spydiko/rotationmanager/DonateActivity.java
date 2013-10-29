b22dc3e952b2ce9caa4bade52534d36c8450bbf4
PuR3v1L <flojim4@gmail.com>
2013-10-29 14:35:57 +0200

    + Proper up navigation

--- AppSpecificOrientation/src/main/java/com/spydiko/rotationmanager/DonateActivity.java
+++ AppSpecificOrientation/src/main/java/com/spydiko/rotationmanager/DonateActivity.java
@@ -10,7 +10,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.ads.Ad;
@@ -185,4 +187,17 @@ public class DonateActivity extends PreferenceActivity implements Preference.OnP
 	public void onAdColonyAdStarted(AdColonyAd adColonyAd) {
 		if (AppSpecificOrientation.LOG) Log.d(TAG, "onAdColonyAdStarted");
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			// Respond to the action bar's Up/Home button
+			case android.R.id.home:
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
+
